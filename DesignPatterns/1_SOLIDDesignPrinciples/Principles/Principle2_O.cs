11420a98c398ec517292a77147f4070a9358a0dd
Michael Catapang <michaelc@aeries.com>
2022-10-28 09:06:15 -0700

    Started Principle 2 notes

---
+++ DesignPatterns/1_SOLIDDesignPrinciples/Principles/Principle2_O.cs
@@ -0,0 +1,106 @@
+﻿namespace Section1_SOLIDDesignPrinciples.Principle2_O
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large, Yuge
+    }
+
+    public class Product
+    {
+        public string? Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string? name, Color color, Size size)
+        {
+            if(name == null)
+            {
+                throw new ArgumentNullException("name");
+            }
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(
+            IEnumerable<Product> products,
+            Size size
+        )
+        {
+            foreach (var p in products)
+            {
+                if(p.Size == size) yield return p;
+            }
+        }
+
+        public IEnumerable<Product> FilterByColor(
+            IEnumerable<Product> products,
+            Color color
+        )
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color) yield return p;
+            }
+        }
+
+        public IEnumerable<Product> FilterBySizeAndColor(
+            IEnumerable<Product> products,
+            Size size,
+            Color color
+        )
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size && p.Color == color) yield return p;
+            }
+        }
+    }
+
+
+
+
+    // Follows the Open-Closed Principle
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        public Color color;
+        public ColorSpecification(Color color)
+        {
+            this.color = color; 
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class BetterFIlter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+            {
+                if (spec.IsSatisfied(i)) yield return i;
+            }
+        }
+    }
+}
--- DesignPatterns/1_SOLIDDesignPrinciples/Program.cs
+++ DesignPatterns/1_SOLIDDesignPrinciples/Program.cs
@@ -1,10 +1,16 @@
 ﻿using Section1_SOLIDDesignPrinciples.Principle1_S;
+using Section1_SOLIDDesignPrinciples.Principle2_O;
 
 namespace Section1_SOLIDDesignPrinciples
 {
     class Demo
     {
         public static void Main(string[] args)
+        {
+            Call_S();
+        }
+
+        public static void Call_S()
         {
             var j = new Journal();
             j.AddEntry("I cried today");
@@ -14,6 +20,21 @@ namespace Section1_SOLIDDesignPrinciples
             var p = new Persistence();
             var filename = @"C:\Users\michaelc\source\repos\Udemy_DesignPatternsCSharpDotNet\journal.txt";
             p.SaveToFile(j, filename, true);
+        }
+
+        public static void Call_O()
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
         }
     }
 }
