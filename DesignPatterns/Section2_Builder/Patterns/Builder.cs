da0ef58d102a11bbd86def191e11bea88dfdea9f
Michael <maocatap@gmail.com>
2023-01-05 00:01:13 -0800

    Fluent Builder with Inheritance

---
+++ DesignPatterns/Section2_Builder/Patterns/Builder.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+
+namespace Section2_Builder.Patterns
+{
+    public class Builder
+    {
+        public static void Run()
+        {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            sb.Clear();
+
+            var words = new[] { "hello", "world" };
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat($"<li>{word}</li>");
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            Console.WriteLine(builder.ToString());
+        }
+
+        private class HtmlElement
+        {
+            public string Name, Text;
+            public List<HtmlElement> Elements = new();
+            private const int indentSize = 2;
+
+            public HtmlElement() { }
+
+            public HtmlElement(string name, string text)
+            {
+                Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+                Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+            }
+
+            private string ToStringImpl(int indent)
+            {
+                var sb = new StringBuilder();
+                var i = new string(' ', indentSize * indent);
+
+                sb.AppendLine($"{i}<{Name}>");
+
+                if (!string.IsNullOrWhiteSpace(Text))
+                {
+                    sb.Append(new string(' ', indentSize * (indent + 1)));
+                    sb.AppendLine(Text);
+                }
+
+                foreach (var e in Elements)
+                {
+                    sb.Append(e.ToStringImpl(indent + 1));
+                }
+
+                sb.AppendLine($"{i}</{Name}>");
+                return sb.ToString();
+            }
+
+            public override string ToString()
+            {
+                return ToStringImpl(0);
+            }
+        }
+
+        private class HtmlBuilder
+        {
+            private readonly string rootName;
+            HtmlElement root = new HtmlElement();
+
+            public HtmlBuilder(string rootName)
+            {
+                this.rootName = rootName;
+                root.Name = rootName;
+            }
+
+            public void AddChild(string childName, string childText)
+            {
+                var e = new HtmlElement(childName, childText);
+                root.Elements.Add(e);
+            }
+
+            public override string ToString()
+            {
+                return root.ToString();
+            }
+
+            public void Clear()
+            {
+                root = new HtmlElement() { Name = rootName };
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Section2_Builder/Patterns/FluentBuilderWithInheritance.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2_Builder.Patterns
+{
+    public class FluentBuilderWithInheritance
+    {
+        public static void Run()
+        {
+            var me = Person.New
+                .Called("dmitri")
+                .WorksAsA("quant")
+                .Build();
+            Console.WriteLine(me);
+        }
+
+        public class Person
+        {
+            public string Name;
+            public string Position;
+
+            public class Builder : PersonJobBuilder<Builder>
+            {
+
+            }
+
+            public static Builder New => new Builder();
+
+            public override string ToString()
+            {
+                return $"{nameof(Name)} {Name}, {nameof(Position)}: {Position}";
+            }
+        }
+
+        public abstract class PersonBuilder
+        {
+            protected Person person = new();
+            public Person Build()
+            {
+                return person;
+            }
+        }
+
+        public class PersonInfoBuilder<SELF> : PersonBuilder 
+            where SELF : PersonInfoBuilder<SELF>
+        {
+            // You can't use the containing type as the return type with inheritance
+            public SELF Called(string name)
+            {
+                person.Name = name;
+                return (SELF)this;
+            }
+        }
+
+        public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>>
+            where SELF : PersonJobBuilder<SELF>
+        {
+            public SELF WorksAsA(string position)
+            {
+                person.Position = position;
+                return (SELF)this;
+            }
+        }
+    }
+}
